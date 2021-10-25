a2cfbb1992dd3e8fe6ad988832ffb8ce17630794
Midnight <arreiskurai@yahoo.com>
2021-10-24 18:57:22 -0700

    Decided to move the sub navigation to the side of the content box.  Some other small styling changes within the DOW

--- src/Components/Anon/AnonSubNav.jsx
+++ src/Components/Anon/AnonSubNav.jsx
@@ -10,19 +10,29 @@ import {BrowserRouter as Router, Link, Switch, Route} from 'react-router-dom';
 function AnonSubNav() {
     return ( 
         <>
-            
-
-            {
-                //#region Switch Router
-            }
-            <Router>
-            <nav class="nav nav-tabs" id="nav-tab" role="tablist">
-                <Link to="/Tutorials_Home" class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Tutorials Home</Link>
-                <Link to="/Dimensions_of_Wellness" class="nav-link" id="nav-dow-tab" data-bs-toggle="tab" href="#nav-dow" role="tab" aria-controls="nav-dow" aria-selected="false">Dimensions of Wellness</Link>
-                <Link to="/State_Operations_Manual" class="nav-link" id="nav-som-tab" data-bs-toggle="tab" href="#nav-som" role="tab" aria-controls="nav-som" aria-selected="false">State Operations Manual</Link>
-                <Link to="/Assessments_Explained" class="nav-link" id="nav-ass-ex-tab" data-bs-toggle="tab" href="#nav-ass-ex" role="tab" aria-controls="nav-ass-ex" aria-selected="false">Assessments Explained</Link>
-                <Link to="/Helpful_Links" class="nav-link" id="nav-links-tab" data-bs-toggle="tab" href="#nav-links" role="tab" aria-controls="nav-links" aria-selected="false">Helpful Links</Link>
-            </nav>
+        <Router>
+        {
+            //#region Nav-Pills
+        }
+        <div className = "row">
+            <div className="col-1 tutorial-nav">
+                <div class="d-flex align-items-start">
+                    <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                        <Link to="/Tutorials_Home"  class="nav-link active" id="v-pills-tut-home-tab" data-bs-toggle="pill" href="#v-pills-tut-home" role="tab" aria-controls="v-pills-tut-home" aria-selected="true">Tutorials</Link>
+                        <Link to="/Dimensions_of_Wellness" class="nav-link" id="v-pills-dow-tab" data-bs-toggle="pill" href="#v-pills-dow" role="tab" aria-controls="v-pills-dow" aria-selected="false">DOW</Link>
+                        <Link to="/State_Operations_Manual" class="nav-link" id="v-pills-som-tab" data-bs-toggle="pill" href="#v-pills-som" role="tab" aria-controls="v-pills-som" aria-selected="false">SOM</Link>
+                        <Link to="/Assessments_Explained" class="nav-link" id="v-pills-assessments-tab" data-bs-toggle="pill" href="#v-pills-assessments" role="tab" aria-controls="v-pills-assessments" aria-selected="false">Assessments</Link>
+                        <Link to="/Helpful_Links" class="nav-link" id="v-pills-links-tab" data-bs-toggle="pill" href="#v-links-assessments" role="tab" aria-controls="v-pills-links" aria-selected="false">Links</Link>
+                    </div>
+                </div>
+            </div>
+        {
+            //#endregion
+        }
+        {
+            //#region Switch Router
+        }
+            <div className ="col-11">
                 <Switch>
                     <Route path="/Tutorials_Home">
                         <Tutorials />
@@ -40,10 +50,12 @@ function AnonSubNav() {
                         <Links />
                     </Route>
                 </Switch>
-            {
-                //$endregion
-            }
-            </Router>
+            </div>
+        {
+            //#endregion
+        }
+        </div>
+        </Router>
         </>
      );
 }
